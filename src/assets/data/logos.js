2c3ed4242686ce19b844696e0fcabf00b54bda44
Rodri Saiz <rodrisaiz@icloud.com>
2026-05-24 23:14:47 +0200

    Adding skills cards

---
+++ src/assets/data/logos.js
@@ -0,0 +1,98 @@
+export const logos = [
+  {
+    name: 'Astro',
+    logo: '/logos/astrojs.svg'
+  },
+  {
+    name: 'Bash',
+    logo: '/logos/bash.svg'
+  },
+  {
+    name: 'Boostrap',
+    logo: '/logos/bootstrap.svg'
+  },
+  {
+    name: 'CSS',
+    logo: '/logos/css.svg'
+  },
+  {
+    name: 'Docker',
+    logo: '/logos/docker.svg'
+  },
+  {
+    name: 'Git',
+    logo: '/logos/git.svg'
+  },
+  {
+    name: 'Github',
+    logo: '/logos/github.svg'
+  },
+  {
+    name: 'Gitlab',
+    logo: '/logos/gitlab.svg'
+  },
+  {
+    name: 'HTML5',
+    logo: '/logos/html5.svg'
+  },
+  {
+    name: 'Javascript',
+    logo: '/logos/javascript.svg'
+  },
+  {
+    name: 'Laravel',
+    logo: '/logos/git.svg'
+  },
+  {
+    name: 'Linux',
+    logo: '/logos/linux.svg'
+  },
+  {
+    name: 'Mongodb',
+    logo: '/logos/mongodb.svg'
+  },
+  {
+    name: 'Mysql',
+    logo: '/logos/mysql.svg'
+  },
+  {
+    name: 'PHP',
+    logo: '/logos/php.svg'
+  },
+  {
+    name: 'Postgresql',
+    logo: '/logos/postgresql.svg'
+  },
+  {
+    name: 'Postman',
+    logo: '/logos/postman.svg'
+  },
+  {
+    name: 'Python',
+    logo: '/logos/python.svg'
+  },
+  {
+    name: 'React',
+    logo: '/logos/react.svg'
+  },
+  {
+    name: 'Redis',
+    logo: '/logos/redis.svg'
+  },
+  {
+    name: 'Rundeck',
+    logo: '/logos/rundeck.svg'
+  },
+  {
+    name: 'Tailwindcss',
+    logo: '/logos/tailwindcss.svg'
+  },
+  {
+    name: 'Slack',
+    logo: '/logos/slack.svg'
+  },
+  {
+    name: 'Vue',
+    logo: '/logos/vue.svg'
+  }
+];
